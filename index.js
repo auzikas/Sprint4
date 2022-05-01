f52b7fafb35a4e7222325ab9bba8d145b1de6796
Pijus Auz <pijusa84@gmail.com>
2022-05-01 20:51:09 +0100

    creating JS file creating function for all buttons, created array for my shopping list and function to push all to array

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+const shoppingInput = document.querySelector(".shopping-list-input");
+const addItemButton = document.querySelector(".shopping-list-submit");
+const listContainer = document.querySelector(".items");
 
+
+let shoppingList = [];
+
+const handleCreatItem = (text) => {
+    const item = document.createElement('div');
+        item.classList.add('item');
+        const content = document.createElement('div');
+        content.classList.add('content');
+        const contentInput = document.createElement('input')
+        contentInput.classList.add('text');
+        contentInput.disabled = true;
+        contentInput.value = text ? text : shoppingInput.value;
+        content.appendChild(contentInput);
+
+        const actions = document.createElement('div');
+        actions.classList.add('actions');
+        const editBtn = document.createElement('button');
+        editBtn.classList.add('edit');
+        editBtn.innerText = 'edit';
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'delete';
+        deleteBtn.classList.add('delete');
+
+        editBtn.addEventListener('click', () => handleEditItem(contentInput, editBtn));
+
+        actions.appendChild(editBtn);
+        actions.appendChild(deleteBtn);
+    
+        item.appendChild(content)
+        item.appendChild(actions)
+        listContainer.appendChild(item);
+        shoppingInput.value = '';
+}
